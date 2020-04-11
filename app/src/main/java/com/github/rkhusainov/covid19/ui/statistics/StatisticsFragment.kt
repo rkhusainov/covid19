0affbbec54b59ffd273c5d5a1186d85a6809865b
Rinat Khusainov <rinat.khusainov.msk@gmail.com>
2020-04-11 21:39:29 +0300

    Add error_view

--- app/src/main/java/com/github/rkhusainov/covid19/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/github/rkhusainov/covid19/ui/statistics/StatisticsFragment.kt
@@ -9,12 +9,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.github.rkhusainov.covid19.R
 import com.github.rkhusainov.covid19.data.model.ResponseItem
 import com.github.rkhusainov.covid19.ui.contract.CountryClickListener
+import kotlinx.android.synthetic.main.error_view.*
 import kotlinx.android.synthetic.main.fragment_statistics.*
 
-class StatisticsFragment : Fragment() {
+class StatisticsFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
     private lateinit var viewModelFactory: CovidViewModelFactory
     private lateinit var viewModel: StatisticsViewModel
@@ -44,11 +46,14 @@ class StatisticsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        swipeRefreshLayout.setOnRefreshListener(this)
+
         setupMVVM()
         statistics_recycler.layoutManager = LinearLayoutManager(context)
         statisticsAdapter = StatisticsAdapter(countryClickListener)
         statistics_recycler.adapter = statisticsAdapter
 
+        updateList()
     }
 
     override fun onStart() {
@@ -68,5 +73,27 @@ class StatisticsFragment : Fragment() {
                     statisticsList ?: emptyList()
                 )
             })
+
+        viewModel.isNetworkError.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                error_view.visibility = View.VISIBLE
+                statistics_recycler.visibility = View.GONE
+            } else {
+                statistics_recycler.visibility = View.VISIBLE
+                error_view.visibility = View.GONE
+            }
+        })
+
+        viewModel.isLoadingData.observe(viewLifecycleOwner, Observer {
+            swipeRefreshLayout.isRefreshing = it
+        })
+    }
+
+    private fun updateList() {
+        viewModel.getStatistics()
+    }
+
+    override fun onRefresh() {
+        updateList()
     }
 }
--- app/src/main/java/com/github/rkhusainov/covid19/ui/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/github/rkhusainov/covid19/ui/statistics/StatisticsViewModel.kt
@@ -1,5 +1,6 @@
 package com.github.rkhusainov.covid19.ui.statistics
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,12 +9,26 @@ import com.github.rkhusainov.covid19.data.repository.ICovidRepository
 import kotlinx.coroutines.launch
 
 class StatisticsViewModel(private val repository: ICovidRepository) : ViewModel() {
-    val statisticsLiveData = MutableLiveData<List<ResponseItem>>()
+    private val _isNetworkError = MutableLiveData(false)
+    private val _isLoadingData = MutableLiveData(false)
+    private val _statisticsLiveData = MutableLiveData<List<ResponseItem>>()
 
     fun getStatistics() {
         viewModelScope.launch {
-            val statistics = repository.getStatistics().response
-            statisticsLiveData.value = statistics
+            _isLoadingData.value = true
+            try {
+                _isNetworkError.value = false
+                val statistics = repository.getStatistics().response
+                _statisticsLiveData.value = statistics
+            } catch (exception: Exception) {
+                _isNetworkError.value = true
+            } finally {
+                _isLoadingData.value = false
+            }
         }
     }
+
+    val isNetworkError: LiveData<Boolean> = _isNetworkError
+    val isLoadingData: LiveData<Boolean> = _isLoadingData
+    val statisticsLiveData: LiveData<List<ResponseItem>> = _statisticsLiveData
 }
